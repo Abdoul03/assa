a5505f5be712c4837dd37fe0de7e1823bf70bfce
Abdoul03 <samabdoul03@gmail.com>
2024-07-04 11:39:46 +0000

    sign_in/sign_up/home/okay

--- assa/connection/views.py
+++ assa/connection/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def sign_in(request):
-    return render(request,'')
+    return render(request,'connection/sign_in.html')
+
 
 def sign_up(request):
-    return render(request,'')
+    return render(request,'connection/sign_up.html')
+
